dc80405ea02c73a753476c7bd5e838618497032b
spuk0 <jabfryyy@gmail.com>
2023-04-24 12:24:22 -0300

    corregido texto en banner

--- app/Views/front/banner.php
+++ app/Views/front/banner.php
@@ -1,6 +1,6 @@
 <div class="banner" style="background-image: url(<?php echo $bannerimg;?>);">
     <div class="container-fluid banner-content d-flex flex-column justify-content-center">
         <h1>HOTEL GUARANI</h1>
-        <p>Lo mejor de Corrientes</p>
+        <p>Lo mejor en Corrientes</p>
     </div>
 </div>
