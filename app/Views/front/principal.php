80080c8e55f5b7935f11da0781f659ecd4bb9916
spuk0 <jabfryyy@gmail.com>
2023-04-24 16:24:02 -0300

    principal terminado, faltarian agregar espacios y estilos

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,3 +1,83 @@
-<div class="container-fluid text-center">
-        
+<div class="container-fluid principal">
+    <div class="bienvenida text-center">
+        <h2>BIENVENIDOS</h2>
+        <p>El hotel donde la comodidad y la elegancia se combinan para crear una experiencia única para nuestros huéspedes.</p>
+        <div class="container ofertas d-flex justify-content-evenly">
+            <figure class="figure">
+                <figcaption class="figure-caption">Ofertas unicas</figcaption>
+                <img class="figure-img img-fluid rounded" 
+                    src="assets/img/icons/tags-icon.svg"
+                    alt="ofertas icon">
+            </figure>
+            <figure class="figure">
+                <figcaption class="figure-caption">Increible comodidad</figcaption>
+                <img class="figure-img img-fluid rounded" 
+                    src="assets/img/icons/bed-icon.svg"
+                    alt="cama comodidad icon">
+            </figure>
+            <figure class="figure">
+                <figcaption class="figure-caption">Y los mejores precios</figcaption>
+                <img class="figure-img img-fluid rounded"
+                    src="assets/img/icons/price-icon.svg"
+                    alt="precios icon">
+            </figure>
+        </div>
+    </div>
+    <section class="container d-flex flex-column align-items-center">
+        <h3>Conta con nosotros</h3>
+        <div class="row">
+            <div class="col-sm-3 mb-3 mb-sm-0">
+                <figure class="figure">
+                    <img class="figure-img img-fluid rounded" 
+                        src="assets/img/hotel/comedor.jpg"
+                        alt="ofertas icon">
+                </figure>
+            </div>
+            <div class="col-sm-5 mb-3 mb-sm-0">
+                <figure class="figure">
+                    <img class="figure-img img-fluid rounded" 
+                        src="assets/img/hotel/sala-estar.jpg"
+                        alt="ofertas icon">
+                </figure>
+            </div>
+            <div class="col-sm-4">
+                <div class="card w-75 mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Deleitate:</h5>
+                        <p class="card-text">Un trato profesional pero totalmente familiar. En nuestro hotel encontraras detalles, mimo y dedicación, para que te sientas como en tu propia casa.</p>
+                        <p class="card-text">Disfruta de tus días de descanso en el corazón de la Ciudad y no dudes en consúltanos sobre la multitud de servicios que podemos ofrecerte.</p>
+                        <a href="#" class="btn btn-primary">Más sobre los servicios</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container d-flex flex-column align-items-center">
+        <h3>Servicios destacados</h3>
+        <div class="servicios container d-flex justify-content-evenly text-center">
+            <figure class="figure">
+                <img class="figure-img img-fluid rounded"
+                    width="40" 
+                    src="assets/img/icons/wifi-icon.svg"
+                    alt="wifi icon">
+                <figcaption class="figure-caption">Wi-Fi</figcaption>
+            </figure>
+
+            <figure class="figure">
+                <img class="figure-img img-fluid rounded"
+                    width="40" 
+                    src="assets/img/icons/parking-icon.svg"
+                    alt="estacionamiento parking comodidad icon">
+                <figcaption class="figure-caption">Estacionamiento</figcaption>
+            </figure>
+
+            <figure class="figure">
+                <img class="figure-img img-fluid rounded"
+                    width="40"
+                    src="assets/img/icons/food-icon.svg"
+                    alt="desayuno icon">
+                <figcaption class="figure-caption">Desayuno</figcaption>
+            </figure>
+        </div>
+    </section>
 </div>
