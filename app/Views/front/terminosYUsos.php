f555cf60b934cc7d8508cd58b83a23e89a3d1cf6
spuk0 <jabfryyy@gmail.com>
2023-04-25 11:12:09 -0300

    Terminos y usos terminado

--- app/Views/front/terminosYUsos.php
+++ app/Views/front/terminosYUsos.php
@@ -1,3 +1,55 @@
-<div>
-    <h2>prueba</h2>
+<div class="terminosYUsos container">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title text-center">Términos y Usos</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary text-center">Hotel Corrientes Plaza Hotel</h6>
+            <ol class="list-group list-group-flush list-group-numbered text-justify">
+                <li class="list-group-item fw-bold">Política de reservas:</li>
+                    <ul>
+                        <li>Los clientes pueden hacer una reserva en línea a través de nuestro sitio web o llamando a nuestro centro de reservas.</li>
+                        <li>Se requiere un depósito del 50% del costo total de la reserva para confirmar la reserva.</li>
+                        <li>El pago se puede realizar mediante todos los medios de pago especificados en la pagina.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de cancelación:</li>
+                    <ul>
+                        <li>Se permite cancelar la reserva hasta 72 horas antes de la fecha de llegada sin penalización.</li>
+                        <li>Si la cancelación se realiza dentro de las 72 horas antes de la fecha de llegada, se cobrará una noche de alojamiento.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de privacidad:</li>
+                    <ul>
+                        <li>La información personal del cliente se recopila y utiliza de acuerdo con nuestras políticas de privacidad y protección de datos.</li>
+                        <li>La información personal del cliente no se compartirá con terceros sin su consentimiento explícito.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de uso de las instalaciones:</li>
+                    <ul>
+                        <li>Las instalaciones del hotel, como la piscina y el gimnasio, están abiertas de 7:00 a 22:00.</li>
+                        <li>Los huéspedes deben seguir las normas y regulaciones para el uso de las instalaciones, como el uso de toallas y el uso de ropa adecuada.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de mascotas:</li>
+                    <ul>
+                        <li>No se permiten mascotas en las instalaciones del hotel, excepto perros guía para personas con discapacidades.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Responsabilidades del hotel y del cliente:</li>
+                    <ul>
+                        <li>El hotel no se hace responsable por la pérdida o daño a la propiedad del cliente, a menos que sea causado por negligencia por parte del hotel.</li>
+                        <li>El cliente es responsable de cualquier daño causado a las instalaciones del hotel durante su estadía.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de seguridad:</li>
+                    <ul>
+                        <li>El hotel tiene medidas de seguridad en su lugar, incluyendo cámaras de seguridad y personal de seguridad en el sitio.</li>
+                        <li>En caso de emergencia, se proporcionará información y asistencia a los huéspedes para garantizar su seguridad.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de tarifas y precios:</li>
+                    <ul>
+                        <li>Las tarifas del hotel son en dólares estadounidenses y están sujetas a impuestos y cargos adicionales.</li>
+                        <li>Los cargos adicionales pueden incluir cargos por estacionamiento, servicio de habitaciones y otros servicios adicionales.</li>
+                    </ul>
+                <li class="list-group-item fw-bold">Política de conducta:</li>
+                    <ul>
+                        <li>Se espera que los clientes del hotel se comporten de manera respetuosa y sigan las normas de conducta del hotel.</li>
+                        <li>Se prohíbe cualquier comportamiento inapropiado o ilegal, como el consumo de drogas, la prostitución y el acoso.</li>
+                    </ul>
+            </ol>
+        </div>
+    </div>
 </div>
